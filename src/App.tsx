e909b82481617433d7a04be525dc6cd2dd291e9a
IgnacioEnriqueArra <ignacioarra.it@gmail.com>
2026-04-13 21:10:54 -0300

    Fix missing useEffect import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AppProvider, useAppContext } from './AppContext';
 import { Feed } from './components/Feed';
 import { IdeaDetail } from './components/IdeaDetail';
